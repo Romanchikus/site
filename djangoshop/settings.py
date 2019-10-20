d3dc62bbd00aec5b93b4c88371d37489231deb79
romanchikus <46922084+Romanchikus@users.noreply.github.com>
2019-10-20 17:52:55 +0300

    Update settings.py

--- djangoshop/settings.py
+++ djangoshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&19o5b01%wtaix3ny^us-h6i3ym072&y_uibl75i*8&tj$dxga'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
